ebc08a1e1e9f639d3566e778a3ad11cdcd3e755a
Laura Dijkhuizen <laura.w.dijkhuizen@gmail.com>
2020-03-08 14:04:48 +0100

    script pre-run checks1: report if sispmctl is present

--- script.sh
+++ script.sh
@@ -3,10 +3,15 @@
 #output directory
 outdir='/home/laura/timelapse'
 
-# do checkups
-if sispmctl present
+### do checkups
 
 if gphoto2 present
+# is sispmctl present?
+if     ! $(which sispmctl)
+then   echo '\e[31mERROR: install sispmctl to control the lights\e[0m'
+      #exit 1
+else   echo '\e[32sispmctl found \e[0m'
+fi
 
 if camera reachable
 
